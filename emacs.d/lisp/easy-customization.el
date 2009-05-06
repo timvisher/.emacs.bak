776328878cb602dff7b68e021e2e6eea160d90e8
Tim Visher <timothy.visher@fms.treas.gov>
2009-05-06 16:23:14 -0400

    Pulled in the Easy Customization output. EOM

---
+++ emacs.d/lisp/easy-customization.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(global-hl-line-mode t)
+ '(hl-line-sticky-flag nil)
+ '(indicate-empty-lines t)
+ '(mouse-avoidance-mode (quote banish) nil (avoid)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:inherit highlight :background "#002233")))))
