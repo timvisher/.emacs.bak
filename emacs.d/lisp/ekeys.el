6e45db05d38b39dd2565cb169414abc0a3a1df80
Tim Visher <timothy.visher@fms.treas.gov>
2009-05-06 15:30:50 -0400

    Made a lot of Yegge Changes. EOM

--- emacs.d/lisp/ekeys.el
+++ emacs.d/lisp/ekeys.el
@@ -1,3 +1,20 @@
 ;; Implement C-tab for bury-buffer
 (global-set-key (kbd "<C-tab>") 'bury-buffer)
 
+;; Don't type M-x (Yegge)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+
+;; Prefer backward-kill-word to backspace (Yegge)
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+
+;; Bind isearch-*-regexp to easier keys (Yegge)
+(global-set-key "\M-s" 'isearch-forward-regexp)
+(global-set-key "\M-r" 'isearch-backward-regexp)
+
+;; Alias query-replace-regexp (Yegge)
+(defalias 'qrr 'query-replace-regexp)
+
+;; Fill Commands (Yegge)
+(global-set-key "\M-p" 'fill-paragraph)
+(global-set-key "\M-q" 'fill-region)
