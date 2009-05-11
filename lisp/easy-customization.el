823f9d88b2ffbb29d178187a46699e6f2081617a
Tim Visher <timothy.visher@fms.treas.gov>
2009-05-11 15:06:37 -0400

    Did some customization around bookmarks. EOM

--- lisp/easy-customization.el
+++ lisp/easy-customization.el
@@ -3,6 +3,8 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(bookmark-default-file "~/.emacs.d/bookmarks/default.bmk")
+ '(bookmark-save-flag 1)
  '(fill-column 80)
  '(global-hl-line-mode t)
  '(hl-line-sticky-flag nil)
