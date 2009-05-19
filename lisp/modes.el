a12e775a944958d2cec2e85a4071474861f07aea
Tim Visher <tim-visher@mail.sandbox>
2009-05-19 03:51:57 -0400

    Basic Formatting correction. EOM

--- lisp/modes.el
+++ lisp/modes.el
@@ -16,7 +16,7 @@
 (autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
 (setq auto-mode-alist (append '(("\\.\\(frm\\|bas\\|cls\\)$" . visual-basic-mode)) auto-mode-alist))
 
-;;JDEE
+;; JDEE
 
 (setq debug-on-error t)
 
